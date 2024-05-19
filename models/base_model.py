41a7bd80f6a51bbe73d83820c7d912485d689509
kofimuad <iamkofiantwi@outlook.com>
2024-05-19 17:22:42 +0000

    fixed error with base model

--- models/base_model.py
+++ models/base_model.py
@@ -14,8 +14,8 @@ class BaseModel:
     def __init__(self):
         self.id = str(uuid.uuid4())
 
-        self.created_at = created_at.utcnow()
-        self.updated_at = updated_at.utcnow()
+        self.created_at = datetime.utcnow()
+        self.updated_at = datetime.utcnow()
 
     def save(self):
         """
@@ -44,7 +44,7 @@ class BaseModel:
         return ("[{}] ({}) {}".format(class_name, self.id, self.__dict__))
 
 
-"""
+
 if __name__ == '__main__':
     my_model = BaseModel()
     my_model.name = "My Base Model"
@@ -59,4 +59,3 @@ if __name__ == '__main__':
         print("\t[{}] ({}) - {}".format(key,
                                         type(my_model_json[key]),
                                         my_model_json[key]))
-"""
