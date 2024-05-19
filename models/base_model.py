526ce24b5b6313da0acaf03df138e8abb841dc6e
kofimuad <iamkofiantwi@outlook.com>
2024-05-19 13:46:26 +0000

    fixed pycodestyle

--- models/base_model.py
+++ models/base_model.py
@@ -43,6 +43,7 @@ class BaseModel:
         class_name = self.__class__.__name__
         return ("[{}] ({}) {}".format(class_name, self.id, self.__dict__))
 
+
 """
 if __name__ == '__main__':
     my_model = BaseModel()
